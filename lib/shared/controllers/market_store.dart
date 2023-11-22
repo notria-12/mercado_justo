3961358cd6da1c650f8d0b3d4f226d0809cfb4d4
notria-12 <airton.araujo@acad.ifma.edu.br>
2023-11-22 16:58:23 -0300

    feat:limit list of markets to max 5 and sort by distance

--- lib/shared/controllers/market_store.dart
+++ lib/shared/controllers/market_store.dart
@@ -108,6 +108,8 @@ abstract class _MarketStoreBase with Store {
   }
 
   List<Market> getMarketsByName(String name) {
-    return groupMarkets.firstWhere((element) => element[0].name == name);
+    List<Market> markets = groupMarkets.firstWhere((element) => element[0].name == name);
+     markets.sort((a, b) => Geolocator.distanceBetween(positionStore.position!.latitude, positionStore.position!.longitude, a.latitude, a.longitude).compareTo(Geolocator.distanceBetween(positionStore.position!.latitude, positionStore.position!.longitude, b.latitude, b.longitude)));
+     return markets.sublist(0,5);
   }
 }
